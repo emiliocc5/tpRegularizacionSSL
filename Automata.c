fa7b823230a7ed4377d50f441eaa14e94a98ff84
Emilio Caccia Campaner <emilio.caccia@uala.com.ar>
2020-09-27 23:02:12 -0300

    WIP Automata

--- Automata.c
+++ Automata.c
@@ -18,55 +18,58 @@ static char lang_d[] = {'B'};
 static int size_lang_d = 1; */
 
 bool charBelongsV2(char, char*);
-//bool char_belongs(char, char*, int);
+bool char_belongs(char, char*, int);
 char* str_concat(char*, char);
 void word_to_file(char);
 
 enum STATE stateZeroTransitions(char c, char* palabra) {
     enum STATE state = E0;
 
-    if (charBelongsV2(c, languageA))
+    if (char_belongs(c, languageA, 10))
         state = E1;
 
-    if (charBelongsV2(c, languageB))
+    else if (char_belongs(c, languageB, 1))
         state = E2;
     
     return state;
 }
 
 enum STATE stateOneTransitions(char c, char* palabra) {
-    enum STATE state = E1;
+    enum STATE state = E0;
 
-    if (charBelongsV2(c, languageA))
+    if (char_belongs(c, languageA, 10))
         state = E3;
 
-    if (charBelongsV2(c, languageB))
+    if (char_belongs(c, languageB, 1))
         state = E2;
 
-    if (charBelongsV2(c, languageC))
+    if (char_belongs(c, languageC, 1))
         state = E4;
     
     return state;
 }
 
 enum STATE stateTwoTransitions(char c, char* palabra) {
-    enum STATE state = E2;
+    enum STATE state = E0;
     return state;
 }
 
 enum STATE stateThreeTransitions(char c, char* palabra) {
-    enum STATE state = E3;
-//El estado 3 tiene un bucle si llega una a, no lo contemplo en un condicional ya que no cambiaria de estado.
-    if (charBelongsV2(c, languageB))
+    enum STATE state = E0;
+
+    if (char_belongs(c, languageA, 10))
+        state = E3;
+
+    if (char_belongs(c, languageB, 1))
         state = E2;
 
     return state;
 }
 
 enum STATE stateFourTransitions(char c, char* palabra) {
-    enum STATE state = E4;
+    enum STATE state = E0;
 
-     if (charBelongsV2(c, languageD))
+     if (char_belongs(c, languageD, 2))
         state = E2;
 
     return state;
@@ -84,7 +87,7 @@ bool charBelongsV2(char c, char language[]) {
     return false;
 }
 
-/* bool char_belongs(char c, char *lang_x, int size_lang_x) {
+bool char_belongs(char c, char *lang_x, int size_lang_x) {
     
     for (int i=0; i<size_lang_x; i++){
         if (c == lang_x[i]){
@@ -94,7 +97,7 @@ bool charBelongsV2(char c, char language[]) {
     }
     return false;
 }
- */
+ 
 void word_to_file(char c) {
     FILE *fptr;
     fptr = fopen("words_found.txt", "a");
--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 
 #ifdef _WIN32
 #include <conio.h>
-#define clrscr() printf("\e[1;1H\e[2J")
 #else
 #include <stdio.h>
 #define clrscr() printf("\e[1;1H\e[2J")
 #endif
+
+
+
+//TODO, Descartar los que no pertenecen al lenguaje, ya que se meten como basura cuando seguido hay uno que si pertenece.
+
 
 int count = 1;
 void print_word();
@@ -18,58 +22,58 @@ void elimina_archivo();
 
 int main(){
     enum STATE state = E0;
-    bool pop_out = false;
+    bool exit = false;
 
-    char curr_char;
-    char* usr_input;
+    char currentCharacter;
+    char* userInput;
     char* palabra = "";
 
-    usr_input = malloc(5 * sizeof(char));
+    userInput = malloc(5 * sizeof(char));
 
     while (true) {
         //clrscr();
         count = 1;
-        printf("==> Expresion regular a analizar => [0-9]*F|[0-9]\\.[01]?\n");
-        printf("==> Por favor, ingrese la linea de texto a analizar => ");
-        scanf("%s", usr_input);
+        printf("-----ER dada: [0-9]*F|[0-9]\\.[01]?\n");
+        printf("-----Ingrese la cadena a analizar (Centinela: %%): ");
+        scanf("%s", userInput);
         printf("\n\n==> Las palabras del lenguaje a reconocer en la secuencia de texto ingresada son\n\n");
 
-        for (int i = 0; i < (strlen(usr_input)); i++){
-            curr_char = usr_input[i];
+        for (int i = 0; i < (strlen(userInput)); i++){
+            currentCharacter = userInput[i];
 
-            if (curr_char == SENTINEL) {
+            if (currentCharacter == SENTINEL) {
                 if ((state == E2) || (state == E4)) {
                     print_word();
                     state = E0;
                 }
 
-                pop_out = false;
+                exit = false;
                 elimina_archivo();
             }
             else {
-                if (pop_out == false) {
+                if (exit == false) {
                     if (state == E0){
-                        state = stateZeroTransitions(curr_char, palabra);
+                        state = stateZeroTransitions(currentCharacter, palabra);
                     }
 
-                    else if (state==E1){
-                        state = stateOneTransitions(curr_char, palabra);
+                    else if (state == E1){
+                        state = stateOneTransitions(currentCharacter, palabra);
                     }
 
-                    else if (state==E2){
-                        state = stateTwoTransitions(curr_char, palabra);
+                    else if (state == E2){
+                        state = stateTwoTransitions(currentCharacter, palabra);
                     }
 
-                    else if (state==E3){
-                        state = stateThreeTransitions(curr_char, palabra);
+                    else if (state == E3){
+                        state = stateThreeTransitions(currentCharacter, palabra);
                     }
                     
                     else if (state==E4){
-                        state = stateFourTransitions(curr_char, palabra);
+                        state = stateFourTransitions(currentCharacter, palabra);
                     }
                 
                     if (state == E0)
-                        pop_out = true;
+                        exit = true;
                 }
             }
         }
@@ -80,7 +84,7 @@ int main(){
     }
 
     elimina_archivo();
-    free(usr_input);
+    free(userInput);
     return 0;
 }
 
